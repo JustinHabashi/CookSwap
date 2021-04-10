74ed0b4072e61d6119d94ac63e38db494285c87a
hannahmadden-krasnick <hannah.mk23@gmail.com>
2021-04-10 14:56:34 -0700

    playing around

---
+++ cookswapapp/src/components/MenuButton.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class MenuButton extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+}
