8b0498fea9e0c697c1de27d002752b8b2b067707
lrea17 <71747396+lrea17@users.noreply.github.com>
2021-04-10 21:42:11 -0700

    nearby swaps fixed

--- cookswapapp/src/components/NearbySwaps.js
+++ cookswapapp/src/components/NearbySwaps.js
@@ -1,11 +1,7 @@
-export const NearbySwaps = (props) => {
-    return(
-        <div>
-<<<<<<< HEAD
-            NearbySwaps
-=======
-            Near By Swaps
->>>>>>> a382e0cbb5e3e8d1db95914bb1ee90ca236367a5
-        </div>
-    )
-}
+export const NearbySwaps = (props) => (
+    <div>
+        <header className="App-pages">
+        Nearby Swaps
+        </header>
+    </div>
+)
