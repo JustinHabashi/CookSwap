208fd0dddec5ec882f1453a94613961dbd66d01a
lrea17 <71747396+lrea17@users.noreply.github.com>
2021-04-10 22:22:23 -0700

    meal ideas updated

--- cookswapapp/src/components/MealIdeas.js
+++ cookswapapp/src/components/MealIdeas.js
@@ -3,11 +3,38 @@ import banana from '../buritto.jpeg'
 export const MealIdeas = (props) => (
     <div>
         <header className="App-pages">
-        <p>Meal Ideas</p>
+        <p><b>Meal Ideas</b></p>
         <div>
         <img src={banana} className="images" alt="logo" />
+        <p><b>Buritto Bowl </b> </p>
         </div>
+        <body className="App-text">
+        <p><b>INGREDIENTS:</b>
+        <p>1 cup uncooked rice</p>
+        <p>1 cup salsa, homemade or store-bought</p>
+        <p>3 cups chopped Romaine lettuce</p>
+        <p>1 (15.25-ounce) can whole kernel corn, drained</p>
+        <p>1 (15-ounce) black beans, drained and rinsed</p>
+        <p>2 Roma tomatoes, diced</p>
+        <p>1 avocado, halved, seeded, peeled and diced</p>
+        <p>2 tablespoons chopped fresh cilantro leaves</p>
+        <p> <b>FOR THE CHIPOTLE CREAM SAUCE </b></p>
+        <p>1 cup sour cream</p>
+        <p>1 tablespoon chipotle paste</p>
+        <p>1 clove garlic, pressed</p>
+        <p>Juice of 1 lime</p>
+        <p>1/4 teaspoon salt, or more, to taste</p>
+        <p><b>DIRECTIONS:</b> To make the chipotle cream sauce, whisk together sour cream, chipotle paste, garlic, lime juice and salt; set aside.
+        In a large saucepan of 1 1/2 cups water, cook rice according to package instructions; let cool and stir in salsa; set aside.
+        To assemble the bowls, divide rice mixture into serving bowls; top with lettuce, corn, black beans, tomatoes, avocado and cilantro.
+        Serve immediately, drizzled with chipotle cream sauce. </p>
+    <p> .  </p>
+    <p> .</p>
+    <p> . </p>
+        </p>
+        </body>
         </header>
+        
         
         
     </div>
