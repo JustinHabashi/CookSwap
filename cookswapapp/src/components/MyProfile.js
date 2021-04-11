0fa83186a9db6b38c6c2f6f3ad731098786402f0
hannahmadden-krasnick <hannah.mk23@gmail.com>
2021-04-10 22:11:39 -0700

    revert of nearbyswaps

--- cookswapapp/src/components/MyProfile.js
+++ cookswapapp/src/components/MyProfile.js
@@ -1,8 +1,25 @@
-
 export const MyProfile = (props) => (
     <div>
-        <header className="App-pages">
-        My Profile
-        </header>
+        <p>
+            Welcome to CookSwap
+        </p>
+        <form>
+                <label>
+                    First Name:
+                    <input type="text" name="first-name" />
+                </label>
+                <br />
+                <label>
+                    Last Name:
+                    <input type="text" name="last-name" />
+                </label>
+                <br />
+                <label>
+                    Address:
+                    <input type="text" name="address" />
+                </label>
+                <br />
+            <input type="submit" value="Submit" />
+        </form>
     </div>
 )
