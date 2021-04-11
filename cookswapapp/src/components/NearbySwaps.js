aaee9d0b577d20cd72eaebdf4f75afadbf90cc68
lrea17 <71747396+lrea17@users.noreply.github.com>
2021-04-10 23:04:21 -0700

    updated nearby swaps

--- cookswapapp/src/components/NearbySwaps.js
+++ cookswapapp/src/components/NearbySwaps.js
@@ -1,12 +1,8 @@
 export const NearbySwaps = (props) => (
     <div>
         <header className="App-pages">
-<<<<<<< HEAD
         <b>Nearby Swaps</b>
-=======
-        Nearby Swaps
-        Imagine a map here, with a location pin!!
->>>>>>> 384b031d268b34ec39900d69c1c993f7fc0e07c5
+        <p> Imagine a map here, with a location pin!! </p>
         </header>
     </div>
 )
